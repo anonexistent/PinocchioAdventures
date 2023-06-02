af0d94b46b154efe76a8f4fea22846a38629525e
anonexistent <90211791+anonexistent@users.noreply.github.com>
2023-06-02 13:44:08 +0500

    mySql no work

--- Assets/Scripts/ChunkPlacer.cs
+++ Assets/Scripts/ChunkPlacer.cs
@@ -48,7 +48,10 @@
 //    }
 //}
 
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class ChunkPlacer : MonoBehaviour
@@ -73,13 +76,54 @@ public class ChunkPlacer : MonoBehaviour
 
     void Update()
     {
-        // 90% curChunck
+        // 80% curChunck
         if (Player.position.x > (CurrentChunks[CurrentChunks.Count - 1].endChunk.position.x) * (0.8f + temp))
         {
-            if (temp >= 0.09f) temp = 0;
+            //if (temp >= 0.9f) temp = 0;
             temp += 0.001f;
             SpawnNewChunk();
             for (int i = 0; i < curObjsCount; i++) objsSpawner.ReplaceObject(CurrentChunks[^1]);
+        }
+    }
+
+    public class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        public string Server { get; set; }
+        public string DatabaseName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        private MySqlConnection Connection { get; set; }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(DatabaseName))
+                    return false;
+                string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
+                Connection = new MySqlConnection(connstring);
+                Connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            Connection.Close();
         }
     }
 
@@ -89,7 +133,7 @@ public class ChunkPlacer : MonoBehaviour
         newC.transform.position = CurrentChunks[CurrentChunks.Count - 1].endChunk.position - newC.startChunk.localPosition;
         CurrentChunks.Add(newC);
 
-        if (CurrentChunks.Count > 10)
+        if (CurrentChunks.Count > 15)
         {
             Destroy(CurrentChunks[0].gameObject);
             CurrentChunks.RemoveAt(0);
--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
@@ -66,7 +68,9 @@ public class PlayerHP : MonoBehaviour
         if (HP==0)
         {
             isAlive = false;
-            
+
+            MySqlNewScore();
+
             GameOver();
         }
         hPpanels.transform.GetChild(HP+1).gameObject.SetActive(false);
@@ -99,6 +103,32 @@ public class PlayerHP : MonoBehaviour
         Application.Quit();
     }
 
+    //  TEST mySql
+
+
+    public void MySqlNewScore()
+    {
+        var dbCon = DBConnection.Instance();
+        dbCon.Server = "127.0.0.1";
+        dbCon.DatabaseName = "test";
+        dbCon.UserName = "root";
+        dbCon.Password = "";
+        if (dbCon.IsConnect())
+        {
+            //suppose col0 and col1 are defined as VARCHAR in the DB
+            string query = $"INSERT INTO `users` (`name`) VALUES ('{StarCollector.starCount}')";
+            var cmd = new MySqlCommand(query, dbCon.Connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string someStringFromColumnZero = reader.GetString(0);
+                string someStringFromColumnOne = reader.GetString(1);
+                Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+            }
+            dbCon.Close();
+        }
+    }
+
     public void xxx()
     {
         //HpMinus();
@@ -107,5 +137,46 @@ public class PlayerHP : MonoBehaviour
         Debug.Log("hp--; tryAgainOnClick");
         
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
+public class DBConnection
+{
+    private DBConnection()
+    {
+    }
+
+    public string Server { get; set; }
+    public string DatabaseName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public MySqlConnection Connection { get; set; }
+
+    private static DBConnection _instance = null;
+    public static DBConnection Instance()
+    {
+        if (_instance == null)
+            _instance = new DBConnection();
+        return _instance;
+    }
+
+    public bool IsConnect()
+    {
+        if (Connection == null)
+        {
+            if (String.IsNullOrEmpty(DatabaseName))
+                return false;
+            string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
+            Connection = new MySqlConnection(connstring);
+            Connection.Open();
+        }
+
+        return true;
+    }
+
+    public void Close()
+    {
+        Connection.Close();
     }
 }
